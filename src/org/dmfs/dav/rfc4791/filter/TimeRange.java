cb78a273215961c3001da356c6d7bbdb0a8e8e9d
Marten Gajda <marten@dmfs.org>
2015-05-20 14:24:47 +0200

    Add dependency on rfc5545-date library Add proper support for the time-range CalDAV filter. It accepts timestamps ot DateTime objects and converts them to proper RFC5545 DATETIME values now.

--- src/org/dmfs/dav/rfc4791/filter/TimeRange.java
+++ src/org/dmfs/dav/rfc4791/filter/TimeRange.java
@@ -22,6 +22,10 @@ package org.dmfs.dav.rfc4791.filter;
 import java.io.IOException;
 
 import org.dmfs.dav.FilterBase;
+import org.dmfs.rfc5545.DateTime;
+import org.dmfs.rfc5545.Weekday;
+import org.dmfs.rfc5545.calendarmetrics.CalendarMetrics;
+import org.dmfs.rfc5545.calendarmetrics.GregorianCalendarMetrics;
 import org.dmfs.xmlobjects.ElementDescriptor;
 import org.dmfs.xmlobjects.QualifiedName;
 import org.dmfs.xmlobjects.builder.AbstractObjectBuilder;
@@ -33,16 +37,21 @@ import org.dmfs.xmlobjects.serializer.XmlObjectSerializer.IXmlAttributeWriter;
 /**
  * A filter that limits the results to a certain range of time.
  * <p>
- * FIXME: the serialization of the date-time values is wrong. We need to convert the time stamps to a UTC date string. We need to import the DateTime for that.
+ * TODO: add server side support
  * </p>
  * 
  * @author Marten Gajda <marten@dmfs.org>
  */
 public final class TimeRange extends CalDavFilter
 {
+	private final static CalendarMetrics GREGORIAN_METRICS = new GregorianCalendarMetrics(Weekday.MO, 4);
+
 	private final static QualifiedName ATTRIBUTE_START = QualifiedName.get("start");
 	private final static QualifiedName ATTRIBUTE_END = QualifiedName.get("end");
 
+	/**
+	 * The {@link ElementDescriptor} of the {@link TimeRange} element.
+	 */
 	public final static ElementDescriptor<TimeRange> DESCRIPTOR = ElementDescriptor.register(QualifiedName.get(NAMESPACE, "time-range"),
 		new AbstractObjectBuilder<TimeRange>()
 		{
@@ -50,28 +59,33 @@ public final class TimeRange extends CalDavFilter
 			public void writeAttributes(ElementDescriptor<TimeRange> descriptor, TimeRange object, IXmlAttributeWriter attributeWriter,
 				SerializerContext context) throws SerializerException, IOException
 			{
-				if (object.start != Long.MIN_VALUE)
+				if (object.start != null)
 				{
-					// FIXME: this is wrong and only for testing
-					attributeWriter.writeAttribute(ATTRIBUTE_START, String.valueOf(object.start), context);
+					attributeWriter.writeAttribute(ATTRIBUTE_START, object.start.toString(), context);
 				}
-				if (object.end != Long.MAX_VALUE)
+				if (object.end != null)
 				{
-					// FIXME: this is wrong and only for testing
-					attributeWriter.writeAttribute(ATTRIBUTE_END, String.valueOf(object.end), context);
+					attributeWriter.writeAttribute(ATTRIBUTE_END, object.end.toString(), context);
 				}
 			};
 		});
 
-	public final long start;
-	public final long end;
+	/**
+	 * The start value of this filter. May be <code>null</code> if no start value has been specified.
+	 */
+	private final DateTime start;
+
+	/**
+	 * The end value of this filter. May be <code>null</code> if no end value has been specified.
+	 */
+	private final DateTime end;
 
 
 	/**
 	 * Create a {@link TimeRange} filter for the given start and without a limit for the end value.
 	 * 
 	 * @param start
-	 *            The time in milliseconds of the start of the range or {@link Long#MIN_VALUE} for an open range in the past.
+	 *            The time in milliseconds of the start of the range.
 	 */
 	public TimeRange(long start)
 	{
@@ -85,16 +99,81 @@ public final class TimeRange extends CalDavFilter
 	 * @param start
 	 *            The time in milliseconds of the start of the range or {@link Long#MIN_VALUE} for an open range in the past.
 	 * @param end
-	 *            The time in milliseconds of the end of the range or {@link Long#MAX_VALUE}for an open range in the future.
+	 *            The time in milliseconds of the end of the range or {@link Long#MAX_VALUE} for an open range in the future.
 	 */
 	public TimeRange(long start, long end)
 	{
+		if (end == Long.MAX_VALUE && start == Long.MIN_VALUE)
+		{
+			throw new IllegalArgumentException("at least one of start or end must be given");
+		}
 		if (end <= start)
 		{
 			throw new IllegalArgumentException("start must be before end");
 		}
-		this.start = start;
-		this.end = end;
+		this.start = start == Long.MIN_VALUE ? null : new DateTime(start);
+		this.end = end == Long.MAX_VALUE ? null : new DateTime(end);
+	}
+
+
+	/**
+	 * Create a {@link TimeRange} filter for the given start and without a limit for the end value.
+	 * 
+	 * @param start
+	 *            The {@link DateTime} of the start of the range or <code>null</code> for an open range in the past.
+	 */
+	public TimeRange(DateTime start)
+	{
+		this(start, null);
+	}
+
+
+	/**
+	 * Create a {@link TimeRange} filter for the given start and end date.
+	 * 
+	 * @param start
+	 *            The {@link DateTime} of the start of the range or <code>null</code> for an open range in the past.
+	 * @param end
+	 *            The {@link DateTime} of the end of the range or <code>null</code> for an open range in the future.
+	 */
+	public TimeRange(DateTime start, DateTime end)
+	{
+		if (start == null && end == null)
+		{
+			throw new IllegalArgumentException("at least one of start or end must be given");
+		}
+
+		if (end != null && start != null && !end.after(start))
+		{
+			throw new IllegalArgumentException("start must be before end");
+		}
+
+		if (start != null && start.isFloating() || end != null && end.isFloating())
+		{
+			throw new IllegalArgumentException("start and end dates must be absolute times");
+		}
+
+		if (start != null)
+		{
+			// DateTime is not immutable, so we get or own private copy to ensure it's not modifed afterwards
+			this.start = new DateTime(GREGORIAN_METRICS, start);
+			this.start.shiftTimeZone(DateTime.UTC);
+		}
+		else
+		{
+			this.start = null;
+		}
+
+		if (end != null)
+		{
+			// DateTime is not immutable, so we get or own private copy to ensure it's not modifed afterwards
+			this.end = new DateTime(GREGORIAN_METRICS, end);
+			this.end.shiftTimeZone(DateTime.UTC);
+		}
+		else
+		{
+			this.end = null;
+		}
 	}
 
 
